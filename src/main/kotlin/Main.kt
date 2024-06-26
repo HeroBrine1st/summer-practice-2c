73554e90b6cf310ac631d77b55e116b26cd33b49
HeroBrine1st Erquilenne <pankov.76.tvink@gmail.com>
2024-06-26 14:41:18 +0300

    Add UI for insurance calculation

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,20 +1,14 @@
 package ru.herobrine1st.practice
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import ru.herobrine1st.practice.ui.MainScreen
 
-@Composable
-fun MainScreen() {
-    Column {
-        Button(onClick = { println("test") }) {
-            Text("Test")
-        }
-    }
-}
+/**
+ * Пусть класс-синглтон предоставляет 2 коэффициента для расчета страховой премии за конкретный вид страхования: автомобиль и недвижимость.
+ * Вместе с ним есть 3 класса-калькулятора: ОСАГО, КАСКО и страхование квартиры. Классы-калькуляторы определяют формулу расчета, используя коэффициенты из класса-синглтона.
+ * Формулы придумать самостоятельно.
+ */
 
 fun main() = application {
     Window(onCloseRequest = ::exitApplication) {
